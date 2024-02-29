deb00ef928f41c7e0d84803bdf367d53c55474c0
Rose Ann Austria <roseann1024@gmail.com>
2024-02-29 14:20:22 -0700

    Added api in the dictionary component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function showKeyword(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Looking for ${keyword} meaning.......`);
+    alert(`Looking for ${keyword} definition.......`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(showKeyword);
   }
 
   function handleKeyword(event) {

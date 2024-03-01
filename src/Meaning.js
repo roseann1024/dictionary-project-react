c769bd46a996441c27fe8a6c98231c36af1de020
Rose Ann Austria <roseann1024@gmail.com>
2024-03-01 15:58:00 -0700

    removed the mapping in the definition

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,12 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3 className="text-capitalize">{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+
+      <p>
+        {props.meaning.definitions[0].definition}
+        <br />
+        <em>{props.meaning.definitions[0].example}</em>
+      </p>
     </div>
   );
 }

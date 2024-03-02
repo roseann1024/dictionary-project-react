588c12802673c57ae2c592172bba84aa8e532ebe
Rose Ann Austria <roseann1024@gmail.com>
2024-03-02 10:55:15 -0700

    created synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,24 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-
-      <p>
-        {props.meaning.definitions[0].definition}
-        <br />
-        <em>{props.meaning.definitions[0].example}</em>
-      </p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }

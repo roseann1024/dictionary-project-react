179638f390ea4c8daa3d96e3cdac8d98379968bf
Rose Ann Austria <roseann1024@gmail.com>
2024-03-02 17:43:36 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ export default function App() {
   return (
     <div className="App">
       <Dictionary defaultKeyword="sunset" />
+      <footer className="text-center mt-3">
+        This website was coded by{" "}
+        <a
+          href="https://github.com/roseann1024"
+          target="_blank "
+          rel="noreferrer"
+        >
+          Rose Ann V. Austria
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/roseann1024/dictionary-project-react"
+          target="_blank "
+          rel="noreferrer"
+        >
+          Git-hub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://dictionary-app-react-rva.netlify.app/"
+          target="_blank "
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
